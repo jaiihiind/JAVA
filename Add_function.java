dbf62415be80221302d6a8dd8c4e48d9a39f6d31
Rishav Ray <98175406+jaiihiind@users.noreply.github.com>
2023-04-23 22:23:05 +0530

    Add files via upload

---
+++ Add_function.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+public class Add_function {
+
+
+    public static int  add(int a , int b){      // function  
+        int c = a+b;
+        return c;
+    }
+
+
+    public static void main(String[] args){
+        int ans = add(2,2);
+        System.out.println(ans);
+                
+     }
+    
+}
