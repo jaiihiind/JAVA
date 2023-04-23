b31941848e5e3c497748de27e29257fc9e71c108
Rishav Ray <98175406+jaiihiind@users.noreply.github.com>
2023-04-23 22:21:34 +0530

    Add files via upload

---
+++ sort_array.java
@@ -0,0 +1,14 @@
+package LAB_PROGRAMS;
+import java.util.*; 
+
+public class sort_array {
+    public static void main(String args[]){
+        int arr[] = {1,8,9,5,6,2,7,3,4};
+        System.out.println("Before sorting : "+ Arrays.toString(arr));
+        
+        Arrays.sort(arr);
+        
+        System.out.println("Before sorting : "+ Arrays.toString(arr));
+    }
+    
+}
