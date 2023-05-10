12c80ab8805384bbe05242d19f33936c382f841f
Rishav Ray <98175406+jaiihiind@users.noreply.github.com>
2023-05-11 02:53:16 +0530

    Add files via upload

---
+++ Patterns/Pattern_one.java
@@ -0,0 +1,18 @@
+package Pattern;
+public class Pattern_one {
+    public static void main(String args[]){
+        for(int rows =0; rows<4; rows++){
+        for(int line=0; line<4; line++ ){   //? after one round then => again starts from 0(starting)
+
+            System.out.print("* ");   //take care of the statement (print) not (println)
+        }
+        System.out.println();
+    }
+}   
+}
+
+
+// * * * * 
+// * * * * 
+// * * * * 
+// * * * *
