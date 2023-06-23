f9924efe8ab1275d619928606da5568454f0a2c8
Rishav Ray <98175406+jaiihiind@users.noreply.github.com>
2023-06-23 19:16:15 +0530

    Add files via upload

---
+++ try_catch.java
@@ -0,0 +1,18 @@
+//import java.util.*;
+public class try_catch{
+    public static void main(String args[]){
+        int a =9;
+        int b =0;
+        
+        try{
+            int c = a/b;
+            System.out.println("The result is " + c);
+        }
+        catch(Exception z){
+            System.out.println("There is gadbad in the code");
+
+            System.out.println(z);  //? *** Dont forget to return this 
+        }
+
+    }
+}
