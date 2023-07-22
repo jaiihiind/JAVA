b1cc49365ff175937024722912924bd22b5e04a2
Rishav Ray <98175406+jaiihiind@users.noreply.github.com>
2023-07-22 07:51:47 +0530

    Add files via upload

--- File_handling.java
+++ File_handling.java
@@ -1,7 +1,9 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-//import java.util.*;
+
+import java.util.*;               //dont use this here , insert all the libraries seperately
+
 public class File_handling {
     public static void main(String args[]){
 
@@ -31,6 +33,28 @@ try{
         }
 
 
+
+//todo        *** READING A FILE ***
+
+        File x = new File("naya_file.txt");
+        try{
+            Scanner sc = new Scanner(x);
+            while (sc.hasNextLine()){
+                String line = sc.nextLine();
+                System.out.println(line);
+            }
+            sc.close();
+        }
+        catch(IOException z){
+            z.printStackTrace();
+        }
+
+
+
+
+
+
+
 //todo        *** DELETING A FILE ***
     //     File naya_file = new File("naya_file.txt");
     //     if(naya_file.delete()){
@@ -42,3 +66,4 @@ try{
      }
     
 }
+
